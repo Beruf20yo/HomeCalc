0fe055b646bcff0acd99191ce3950eedeb7ab357
RinatG <rinat_galimov_1992@mail.ru>
2023-08-11 13:06:30 +0300

    add try-catch to Calculator.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
-        try {
-            int a = calc.plus.apply(1, 2);
-            int b = calc.minus.apply(1,1);
-            int c = calc.devide.apply(a, b); // Данный код не работает, потому что здесь мы делим на ноль (b = 0)
-            calc.println.accept(c);
-        }catch (ArithmeticException e){
-            System.out.println("Ошибка математического характера: " + e.getMessage());
-        }
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b); // Данный код не работает, потому что здесь мы делим на ноль (b = 0)
+        calc.println.accept(c);
+
 
     }
 
