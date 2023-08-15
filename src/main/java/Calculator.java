e2a148e4413b5f47d3233ce8b5712da928b8e713
RinatG <rinat_galimov_1992@mail.ru>
2023-08-15 11:32:19 +0300

    micro change

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,17 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> {
-        try {
-            if (y == 0) {
-                throw new ArithmeticException("Ошибка математического характера");
-            }
-            return x / y;
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-            return 0;
-        }
-    };
+    BinaryOperator<Integer> devide = (x, y) -> (y == 0) ?  Integer.MAX_VALUE :  x / y;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
